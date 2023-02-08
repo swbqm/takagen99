4b9ff6e1d785bbaab6b11b30b21b1f447ecacf7c
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-02-09 00:34:41 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -63,7 +63,7 @@ public class App extends MultiDexApplication {
         putDefault(HawkConfig.HOME_REC, 1);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
         putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
         putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
-		putDefault(HawkConfig.HOME_SEARCH_POSITION, 0);      //主页搜索按钮位置 0=下方 ,1=上方
+//		putDefault(HawkConfig.HOME_SEARCH_POSITION, 0);      //主页搜索按钮位置 0=下方 ,1=上方
 //        putDefault(HawkConfig.HOME_NUM, 2);       // 历史记录数量
 //        putDefault(HawkConfig.DOH_URL, 2);        // DNS
 //        putDefault(HawkConfig.SEARCH_VIEW, 1);    // 0=文字列表搜索结果 ,1= 图片搜索结果
